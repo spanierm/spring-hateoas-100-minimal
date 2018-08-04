7457e5ee6da0697ae6de39c96a870fb5c2a1511a
Markus Spanier <spanierm@gmx.net>
2018-08-04 12:11:34 +0200

    Successful build with default 0.0.25.RELEASE version

---
+++ src/main/java/com/example/springhateoas100minimal/SpringHateoas100MinimalApplication.java
@@ -0,0 +1,11 @@
+package com.example.springhateoas100minimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SpringHateoas100MinimalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringHateoas100MinimalApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/springhateoas100minimal/SpringHateoas100MinimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhateoas100minimal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class SpringHateoas100MinimalApplicationTests {
+
+  @Test public void contextLoads() {
+  }
+
+}
